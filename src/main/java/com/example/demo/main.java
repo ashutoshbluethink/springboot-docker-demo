8d6e81f2f200d9cca2126d13aa2029c0ab260aae
Ashutosh Kushwaha <ashutosh2bluethink@gmail.com>
2025-09-07 02:20:25 +0530

    This is intelij git commit test

---
+++ src/main/java/com/example/demo/main.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+public class main {
+    public static void main(String[] args) {
+
+        for(int i=1 ; i <=5 ; i++){
+           // System.out.println("*");
+            for(int j=1 ; j<=5 ; j++){
+                if(i == j){
+                    System.out.print(i);
+                }else {
+                    System.out.print("*");
+
+                }
+
+            }
+
+            System.out.println();
+        }
+    }
+}
